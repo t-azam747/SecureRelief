98e20f7c6e8807e6e292c407816c120166c06461
Vansh Deo <vanshdeo5.mathsg@gmail.com>
2026-01-03 21:07:42 +0530

    Fixed 404 error

--- frontend-next/src/app/page.tsx
+++ frontend-next/src/app/page.tsx
@@ -3,11 +3,14 @@
 import React from 'react';
 import Layout from '@/components/Layout/Layout';
 import HomePage from '@/components/Home/HomePage';
+import { redirect } from 'next/navigation';
 
 export default function Page() {
   return (
-    <Layout fullWidth={true}>
-      <HomePage />
-    </Layout>
+    // <Layout fullWidth={true}>
+    //   <HomePage />
+    // </Layout>
+
+    redirect('./components/Home/HomePage')
   );
 }
